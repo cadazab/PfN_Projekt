c07868d44611ac690124fdddb97504ddc579a3d2
Karim <6merkl@informatik.uni-hamburg.de>
2018-06-12 17:15:13 +0200

    included structs in protein.h

--- protein.h
+++ protein.h
@@ -1,10 +1,24 @@
 #ifndef PROTEIN_H
 #define PROTEIN_H
 
-typedef struct Protein Protein;
+typedef struct {
+    char *name;
+    Residue *residues; 
+    Atom *atoms;
+    Atom *cAlphas;
+    unsigned long nr_residues;
+    unsigned long nr_atoms;
+} Protein;
 
-typedef struct Residue Residue;
+typedef struct {
+    char *name;
+    Atom *atoms;
+    unsigned long nr_atoms;
+} Residue;
 
-typedef struct Atom Atom;
+typedef struct {
+    char *name;
+    double *coordinates;
+}  Atom;
 
 #endif
