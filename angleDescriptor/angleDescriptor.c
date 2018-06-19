474cb901f2f668b0a28be7191639431390026a29
tnt <aydin@debian>
2018-06-19 17:35:36 +0200

    Finished math stuff and wrote a test in main

--- angleDescriptor/angleDescriptor.c
+++ angleDescriptor/angleDescriptor.c
@@ -1,40 +1,78 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
-struct Vektor{
+#define PI 3.14159265
+
+typedef struct Vector{
   double x;
   double y;
   double z;
+}Vector;
+
+Vector cross_product(Vector a, Vector b){
+  Vector result;
+  result.x = a.y*b.z - a.z*b.y;
+  result.y = a.z*b.x - a.x*b.z;
+  result.z = a.x*b.y - a.y*b.x;
+  return result;
 }
 
-Vektor Kreuzprodukt(Vektor a, Vektor b){
-  Vektor ergebnis = new Vektor;
-  ergebnis.x = a.y*b.z - a.z*b.y;
-  ergebnis.y = a.z*b.x - a.x*b.z;
-  ergebnis.z = a.x*b.y - a.y*b.x;
-  return ergebnis;
+double scalar_product(Vector a, Vector b){
+  double result;
+  result = a.x*b.x + a.y*b.y + a.z*b.z;
+  return result;
 }
 
-double Skalarprodukt(Vektor a, Vektor b){
-  double ergebnis;
-  ergebnis = a.x*b.x + a.y*b.y + a.z*b.z;
-  return ergebnis;
+double absi(Vector a){
+  double result;
+  result = sqrt(pow(a.x,2) + pow(a.y,2) + pow(a.z,2));
+  return result;
 }
 
-double Betrag(Vektor a){
-  double ergebnis;
-  ergebnis = sqrt(pow(a.x,2) + pow(a.y,2) + pow(a.z,2));
-  return ergebnis;
+Vector normalization(Vector a){
+  double abs = absi(a);
+  Vector result;
+  result.x = a.x/abs;
+  result.y = a.y/abs;
+  result.z = a.z/abs;
+  return result;
 }
 
-Vektor Normierung(Vektor a){
-  double betrag = Betrag(a);
-  Vektor ergebnis = new Vektor;
-  ergebnis.x = a.x/betrag;
-  ergebnis.y = a.y/betrag;
-  ergebnis.z = a.z/betrag;
-  return ergebnis;
+Vector difference(Vector a, Vector b){
+  Vector result;
+  result.x = a.x - b.x;
+  result.y = a.y - b.y;
+  result.z = a.z - b.z;
+  return result;
 }
 
+double angle(Vector a, Vector b, Vector c, Vector d){
+  Vector v1 = difference(a,b);
+  Vector v2 = difference(b,c);
+  Vector v3 = difference(c,d);
+  return acos(scalar_product(normalization(cross_product(v1,v2)),normalization(cross_product(v2,v3)))) * 180 / PI;
+}
+
+int main(int argc, char *argv[]){
+  Vector a;
+  Vector b;
+  Vector c;
+  Vector d;
+  a.x=1;
+  a.y=0;
+  a.z=1;
+  b.x=0;
+  b.y=0;
+  b.z=0;
+  c.x=0;
+  c.y=1;
+  c.z=0;
+  d.x=0;
+  d.y=1;
+  d.z=1;
+  printf("angle:%f°\n",angle(a,b,c,d));
+  return EXIT_SUCCESS;
+}
 
   
