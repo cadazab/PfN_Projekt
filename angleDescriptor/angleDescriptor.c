e21baf56fdc766bd91e71cd01b8cd4c4501446b9
tnt <aydin@debian>
2018-06-26 16:54:55 +0200

    finished functions

--- angleDescriptor/angleDescriptor.c
+++ angleDescriptor/angleDescriptor.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include <math.h>
 
-#define PI 3.14159265
+#include "../protein.h"
+#include "../nGram.h"
 
 typedef struct Vector{
   double x;
   double y;
   double z;
 }Vector;
+
+typedef struct Angle{
+  double angle;
+  double distance;
+}Angle;
 
 Vector cross_product(Vector a, Vector b){
   Vector result;
@@ -24,14 +30,14 @@ double scalar_product(Vector a, Vector b){
   return result;
 }
 
-double absi(Vector a){
+double abs_val(Vector a){
   double result;
   result = sqrt(pow(a.x,2) + pow(a.y,2) + pow(a.z,2));
   return result;
 }
 
 Vector normalization(Vector a){
-  double abs = absi(a);
+  double abs = abs_val(a);
   Vector result;
   result.x = a.x/abs;
   result.y = a.y/abs;
@@ -51,7 +57,37 @@ double angle(Vector a, Vector b, Vector c, Vector d){
   Vector v1 = difference(a,b);
   Vector v2 = difference(b,c);
   Vector v3 = difference(c,d);
-  return acos(scalar_product(normalization(cross_product(v1,v2)),normalization(cross_product(v2,v3)))) * 180 / PI;
+  return acos(scalar_product(normalization(cross_product(v1,v2)),normalization(cross_product(v2,v3)))) * 180 / M_PI;
+}
+
+Vector create_vector(Atom atom){
+  Vector result;
+  result.x = atom.x;
+  result.y = atom.y;
+  result.z = atom.z;
+  return result;
+}
+
+Angle get_angle(Protein protein){
+  Angle* result;
+  unsigned long nr_ngrams = numberOfNGrams(&protein,6);
+  result = malloc(sizeof(*result) * nr_ngrams);
+  nGram* n_grams = generateNGrams(protein,6);
+  unsigned int i;
+  for(i = 0; i < nr_ngrams; i++){
+    Vector v1,v2,v3,v6;
+    v1 = create_vector(n_grams[i].cAlphas[0]);
+    v2 = create_vector(n_grams[i].cAlphas[1]);
+    v3 = create_vector(n_grams[i].cAlphas[2]);
+    v6 = create_vector(n_grams[i].cAlphas[5]);
+    result[i].angle = angle(v1,v2,v3,v6);
+    result[i].distance = abs_val(difference(v1,v6));
+  }
+  return result;
+}
+
+void free_angle(*Angle angle){
+  free(angle);
 }
 
 int main(int argc, char *argv[]){
