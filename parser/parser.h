369444f7a885b1db7da9cd7528ff100b6d019298
Karim <6merkl@informatik.uni-hamburg.de>
2018-07-05 22:12:56 +0200

    latest  version from parser branch with little changes

--- parser/parser.h
+++ parser/parser.h
@@ -1,9 +1,10 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "protein.h"
+#include "../protein.h"
 
-Protein * parse(char * filename);
+Protein * parse(const char * filename);
 
 void freeProteinStruct(Protein * protein);
+char * readFromFile(const char *filename, unsigned long *filesize);
 #endif
