ccb093982fb1051b690c4382a97772e1f7510070
Karim <6merkl@informatik.uni-hamburg.de>
2018-07-10 12:49:32 +0200

    merge parser

--- parser/parser.h
+++ parser/parser.h
@@ -5,6 +5,9 @@
 
 Protein * parse(const char * filename);
 
+/*
+* Takes a Protein struct and frees itself and every content of it.
+*/
 void freeProteinStruct(Protein * protein);
 char * readFromFile(const char *filename, unsigned long *filesize);
 #endif
